9b718449c0ab3cf16dcea28a2534a15a630cffba
Dorota Wojcik <dorota.wojcik@aiven.io>
2024-01-26 15:37:39 +0100

    add clickhouse credentials integrations docs

--- sidebars.ts
+++ sidebars.ts
@@ -1321,6 +1321,7 @@ const sidebars: SidebarsConfig = {
                 'products/clickhouse/concepts/strings',
                 'products/clickhouse/concepts/federated-queries',
                 'products/clickhouse/concepts/clickhouse-tiered-storage',
+                'products/clickhouse/concepts/data-integration-overview',
               ],
             },
             {
