bca28fb4ef5b0ff05acf3402810658a2ad72838b
ArthurFlageul <arthur.flageul-marquez@aiven.io>
2024-03-06 11:40:22 -0700

    update: extract css for all badges

--- src/components/Badges/LimitedBadge/index.tsx
+++ src/components/Badges/LimitedBadge/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import clsx from 'clsx';
 import styles from './styles.module.css';
+import stylesGeneric from '../styles.module.css';
 
 export default function LimitedBadge(): JSX.Element {
-  return <span className={styles.badge}>Limited availabilty</span>;
+  return (
+    <span className={clsx(stylesGeneric.badge, styles.badge)}>
+      Limited availabilty
+    </span>
+  );
 }
--- src/components/Badges/ProBadge/index.tsx
+++ src/components/Badges/ProBadge/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import clsx from 'clsx';
 import styles from './styles.module.css';
+import stylesGeneric from '../styles.module.css';
 
 export default function ProBadge(): JSX.Element {
-  return <span className={styles.badge}>pro</span>;
+  return <span className={clsx(stylesGeneric.badge, styles.badge)}>pro</span>;
 }
