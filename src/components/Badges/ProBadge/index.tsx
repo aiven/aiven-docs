e431ac723964f9c3c0c9353177e3c79353009fca
ArthurFlageul <arthur.flageul-marquez@aiven.io>
2024-03-06 11:40:22 -0700

    UPdate: extract css for all badges

--- src/components/Badges/ProBadge/index.tsx
+++ src/components/Badges/ProBadge/index.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 import stylesGeneric from '../styles.module.css';
+import {Tooltip} from 'react-tooltip';
 
 export default function ProBadge(): JSX.Element {
-  return <span className={clsx(stylesGeneric.badge, styles.badge)}>pro</span>;
+  return (
+    <>
+      <a
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content="This feature is part of Aiven Enterprise."
+        data-tooltip-offset={-3}>
+        <span className={clsx(stylesGeneric.badge, styles.badge)}>Pro</span>
+      </a>
+      <Tooltip id="my-tooltip" className={stylesGeneric.aivenTooltip} />
+    </>
+  );
 }
