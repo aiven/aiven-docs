68be39ad9a9d1e71179441a9af94b7d31b2208d4
Arthur <arthur.flageul-marquez@aiven.io>
2024-05-13 14:44:20 +0200

    update: extract css from Badge component (#195)

--- src/components/Badges/EarlyBadge/index.tsx
+++ src/components/Badges/EarlyBadge/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import clsx from 'clsx';
 import styles from './styles.module.css';
+import stylesGeneric from '../styles.module.css';
 
 export default function EarlyBadge(): JSX.Element {
-  return <span className={styles.badge}>Early availabilty</span>;
+  return (
+    <span className={clsx(stylesGeneric.badge, styles.badge)}>
+      Early availabilty
+    </span>
+  );
 }
