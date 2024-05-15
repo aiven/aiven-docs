7ffb7176f0bf02c4fda2494b4a68d92db6760642
Stacey Salamon <111294980+staceysalamon-aiven@users.noreply.github.com>
2024-05-15 16:16:14 +0200

    update: remove troubleshooting section from Terraform (#202)

--- sidebars.ts
+++ sidebars.ts
@@ -523,17 +523,6 @@ const sidebars: SidebarsConfig = {
                 'tools/terraform/howto/vpc-peering-gcp',
               ],
             },
-            {
-              type: 'category',
-              label: 'Troubleshooting',
-              link: {
-                type: 'doc',
-                id: 'tools/terraform/reference/troubleshooting',
-              },
-              items: [
-                'tools/terraform/reference/troubleshooting/private-access-error',
-              ],
-            },
           ],
         },
         'tools/kubernetes',
