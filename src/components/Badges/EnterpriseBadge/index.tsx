a1f373ec3b1ef2d9bbf27e40c4980310db3baab7
ArthurFlageul <arthur.flageul-marquez@aiven.io>
2024-07-29 10:51:26 +0200

    fix: badge uniformity

--- src/components/Badges/EnterpriseBadge/index.tsx
+++ src/components/Badges/EnterpriseBadge/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import clsx from 'clsx';
 import styles from './styles.module.css';
+import stylesGeneric from '../styles.module.css';
 
 export default function EnterpriseBadge(): JSX.Element {
-  return <span className={styles.badge}>enterprise</span>;
+  return (
+    <span className={clsx(stylesGeneric.badge, styles.badge)}>enterprise</span>
+  );
 }
