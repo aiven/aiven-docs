8e5258086f82d70afa4bf8f6631442e6a884734c
Harshini Rangaswamy <harshini.rangaswamy@aiven.io>
2024-08-08 16:41:50 +0200

    fix: update MySQL Debezium Connector doc to version 2.5

--- src/components/ConsoleIcons/index.tsx
+++ src/components/ConsoleIcons/index.tsx
@@ -371,6 +371,12 @@ export default function ConsoleLabel({name}): ReactElement {
           <b>Tiered storage</b>
         </>
       );
+    case 'editconfig':
+      return (
+        <>
+          <ConsoleIconWrapper icon={ConsoleIcons.edit} /> <b>Edit</b>
+        </>
+      );
     default:
       return (
         <span style={{padding: 2, backgroundColor: 'red', color: '#ffffff'}}>
