ef439304a4374dde0f17cce5b602428040522405
Arthur <arthur.flageul-marquez@aiven.io>
2024-10-18 14:57:26 +0200

    add: link checker job (#512)

---
+++ scripts/check_links.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+excludeList=(
+    "aiven.io/?!docs"
+	  "aiven.io/docs/assets"
+	  "aiven.io/docs/images"
+    "aiven.io/community"
+    "aiven.io/build"
+    ".*cdn.sanity.io"
+    ".*console.cloud.google.com"
+    ".*docs.npmjs.com"
+    ".*github.com"
+    ".*linkedin.com"
+    ".*maven.apache.org"
+    ".*uptime.aiven.io"
+    ".*docker.com"
+    ".*php.net"
+  )
+
+acceptedResponses="--accepted-status-codes=200..404"
+address="https://aiven.io/docs/"
+bufferSize="--buffer-size=8192"
+limitConnections="--max-connections=9"
+rateLimit="--rate-limit=30"
+redirectionLimit="--max-redirections=5"
+timeout="--timeout=4000"
+
+muffet ${acceptedResponses} \
+       ${bufferSize} \
+       ${excludeList[@]/#/--exclude } \
+       ${limitConnections} \
+       ${rateLimit} \
+       ${redirectionLimit} \
+       ${timeout} \
+       "--color=always" \
+       ${address};
