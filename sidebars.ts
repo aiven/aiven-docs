b8d18d4823bf0f7ded2e9963e917695602fb9502
Harshini Rangaswamy <108724024+harshini-rangaswamy@users.noreply.github.com>
2024-11-26 10:09:35 +0100

    add: Metrics for OpenSearch via Prometheus (#571)

--- sidebars.ts
+++ sidebars.ts
@@ -1709,6 +1709,7 @@ const sidebars: SidebarsConfig = {
 
                 'products/opensearch/reference/restapi-limited-access',
                 'products/opensearch/reference/low-space-watermarks',
+                'products/opensearch/howto/os-metrics',
               ],
             },
             {
