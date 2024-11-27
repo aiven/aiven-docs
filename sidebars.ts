0487ac1659e4f98839e133ac4277857352e85540
Dorota Wojcik <dorota.wojcik@aiven.io>
2024-11-27 17:18:04 +0100

    vpc description draft

--- sidebars.ts
+++ sidebars.ts
@@ -232,7 +232,12 @@ const sidebars: SidebarsConfig = {
             {
               type: 'category',
               label: 'VPCs',
+              link: {
+                type:'doc',
+                id: 'platform/concepts/vpcs',
+              },
               items: [
+                'platform/concepts/vpc-peering',
                 'platform/howto/manage-vpc-peering',
                 'platform/howto/public-access-in-vpc',
                 'platform/howto/vpc-peering-gcp',
