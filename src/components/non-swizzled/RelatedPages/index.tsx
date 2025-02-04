3169edca22b43f027233f536a72f803e7296d3af
ArthurFlag <arthur.flageul-marquez@aiven.io>
2025-02-04 11:52:15 +0100

    feat: related pages component

---
+++ src/components/non-swizzled/RelatedPages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const RelatedPages: React.FC = () => {
+  return <p className={styles.title}>Related pages</p>;
+};
+
+export default RelatedPages;
