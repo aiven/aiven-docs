90fc34d220e91ed45dd608349a6e764d3436a1cd
Bani Rahnama <bani.rahnama@aiven.io>
2025-04-22 17:03:09 +0300

    feat: add kapa AI

---
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import KapaAIButton from '@site/src/components/Buttons/KapaAIButton';
+
+export default {
+  ...ComponentTypes,
+  'custom-kapaAIButton': KapaAIButton,
+};
