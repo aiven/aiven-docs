a7a7f5e33f5b96922a21dfd8a1fe9a01b8b5ff8c
Stacey Salamon <stacey.salamon@aiven.io>
2025-06-17 18:20:14 +0200

    update: add tooltips and links to LA and EA badges

--- src/components/Badges/LimitedBadge/index.tsx
+++ src/components/Badges/LimitedBadge/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import clsx from 'clsx';
+import {Tooltip} from 'react-tooltip';
 import styles from './styles.module.css';
 import stylesGeneric from '../styles.module.css';
 
 export default function LimitedBadge(): JSX.Element {
+  const tooltipId = 'limited-badge-tooltip';
+
   return (
-    <span className={clsx(stylesGeneric.badge, styles.badge)}>
-      Limited availability
-    </span>
+    <>
+      <a
+        href="/docs/platform/concepts/service-and-feature-releases"
+        data-tooltip-id={tooltipId}
+        data-tooltip-content="You can try limited availability features and services by contacting the sales team."
+        data-tooltip-place="top"
+        className={clsx(stylesGeneric.badge, styles.badge)}>
+        Limited availability
+      </a>
+      <Tooltip id={tooltipId} />
+    </>
   );
 }
