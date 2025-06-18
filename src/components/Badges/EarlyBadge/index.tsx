7150a7ddc6393d171739226e88cd31912cb60276
Stacey Salamon <111294980+staceysalamon-aiven@users.noreply.github.com>
2025-06-18 09:50:01 +0200

    update: add tooltips and links to LA and EA badges (#914)

--- src/components/Badges/EarlyBadge/index.tsx
+++ src/components/Badges/EarlyBadge/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import clsx from 'clsx';
+import {Tooltip} from 'react-tooltip';
 import styles from './styles.module.css';
 import stylesGeneric from '../styles.module.css';
 
 export default function EarlyBadge(): JSX.Element {
+  const tooltipId = 'early-badge-tooltip';
+
   return (
-    <span className={clsx(stylesGeneric.badge, styles.badge)}>
-      Early availability
-    </span>
+    <>
+      <a
+        href="/docs/platform/concepts/service-and-feature-releases"
+        data-tooltip-id={tooltipId}
+        data-tooltip-content="Early availability features and services are available for testing and may change."
+        data-tooltip-place="top"
+        className={clsx(stylesGeneric.badge, styles.badge)}>
+        Early availability
+      </a>
+      <Tooltip id={tooltipId} />
+    </>
   );
 }
