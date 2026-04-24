57f9061b272d18f6f8aab71eda1ef3a5f9f9ecb8
Stacey Salamon <stacey.salamon@aiven.io>
2026-04-24 16:33:10 +0200

    make survey banner closeable

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -164,7 +164,7 @@ const config: Config = {
         'Tell us about your experience and help us improve the docs by taking a <a target="_blank" rel="noopener noreferrer" href="https://t.maze.co/443153085">3-minute survey</a>.',
       backgroundColor: '#6F64FF',
       textColor: '#FFFFFF',
-      isCloseable: false,
+      isCloseable: true,
     },
     navbar: {
       title: 'aiven',
