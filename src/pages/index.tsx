1ec9024adfcb61a53eff2eeec450ae88beca3d79
Stacey Salamon <stacey.salamon@aiven.io>
2026-04-28 10:07:55 +0200

    change hero iamge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import Asterisk from '@site/static/images/Asterisk.svg';
-import CurlyBraceLeft from '@site/static/images/Curly brace left.svg';
-import Slash from '@site/static/images/Slash.svg';
+import CurlyBraceRight from '@site/static/images/Curly brace left.svg';
 
 import styles from './index.module.css';
 
@@ -15,24 +14,27 @@ function HomepageHeader() {
   return (
     <header className={styles.heroBanner}>
       <div className={styles.heroDecor} aria-hidden="true">
-        <CurlyBraceLeft className={styles.heroDecoBrace} />
+        <CurlyBraceRight className={styles.heroDecoBrace} />
         <Asterisk className={styles.heroDecoAsterisk} />
-        <Slash className={styles.heroDecoSlash} />
       </div>
       <div className={styles.container}>
-        <Heading as="h1" className={styles.heroTitle}>
-          {siteConfig.title}
-        </Heading>
-        <p className={styles.heroSubtitle}>{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx(
-              'button button--primary button--lg',
-              styles.buttonHome,
-            )}
-            to="/docs/get-started">
-            Get started
-          </Link>
+        <div className={styles.heroGrid}>
+          <div className={styles.heroContent}>
+            <Heading as="h1" className={styles.heroTitle}>
+              {siteConfig.title}
+            </Heading>
+            <p className={styles.heroSubtitle}>{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx(
+                  'button button--primary button--lg',
+                  styles.buttonHome,
+                )}
+                to="/docs/get-started">
+                Get started
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </header>
