a66f557146bea1d0f89240b10ab36588b33e0b5b
Jonas Keeling <jonas.keeling@aiven.io>
2026-07-13 10:54:26 +0200

    update: add AzureSecretProvider docs (#1481)
    Co-authored-by: Harshini Rangaswamy <harshini.rangaswamy@aiven.io>

--- sidebars.ts
+++ sidebars.ts
@@ -894,6 +894,7 @@ const sidebars: SidebarsConfig = {
                           },
                           items: [
                             'products/kafka/kafka-connect/howto/configure-aws-secrets-manager',
+                            'products/kafka/kafka-connect/howto/configure-azure-key-vault',
                             'products/kafka/kafka-connect/howto/configure-hashicorp-vault',
                             'products/kafka/kafka-connect/howto/configure-env-secret-provider',
                           ],
