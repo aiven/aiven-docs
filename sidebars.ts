2da61b9ce632a4c9ebcb935c48f3b3011a5b2a2b
Dorota Wojcik <dorota.wojcik@aiven.io>
2026-08-12 16:28:25 +0200

    remove password encryption doc

--- sidebars.ts
+++ sidebars.ts
@@ -2087,7 +2087,6 @@ const sidebars: SidebarsConfig = {
                     'products/postgresql/reference/pg-connection-limits',
                     'products/postgresql/reference/idle-connections',
                     'products/postgresql/reference/use-of-deprecated-tls-versions',
-                    'products/postgresql/troubleshooting/pg-password-encryption-upgrade',
                     'products/postgresql/troubleshooting/troubleshooting-fatal-out-of-shared-mem',
                   ],
                 },
