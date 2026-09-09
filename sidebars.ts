63bdce6a966dd9b556b8aa34fc3302cf9a86c9b9
Stacey Salamon <stacey.salamon@aiven.io>
2026-09-09 17:31:16 +0200

    update: connect a GitHub account

--- sidebars.ts
+++ sidebars.ts
@@ -435,6 +435,7 @@ const sidebars: SidebarsConfig = {
       items: [
         'products/runtime',
         'products/runtime/deploy-apps',
+        'products/runtime/connect-github-account',
         {
           type: 'category',
           label: 'Manifest files',
